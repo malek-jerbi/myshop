c7dc49f55dc31db7083d9866960354f2487c0180
Malek Jerbi <malek.jerbi@gmail.com>
2021-12-15 11:50:30 -0500

    prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -12,6 +13,16 @@ connectDB()
 const app = express()
 
 app.use('/api/products', productRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve()
+
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+}
 
 app.use(notFound)
 
